72b3931644a0711b96374b3b6df63df23e89b044
NestN1009 <nest.worraphat@hotmail.com>
2024-05-13 14:09:00 +0700

    Create Exercise4_Woraphas_N.py

---
+++ Exercise4_Woraphas_N.py
@@ -0,0 +1,13 @@
+#Score Input
+
+FE = float(input())
+GB = float(input())
+ICS = float(input())
+CP = float(input())
+
+#Score Output
+print("--- Your Score ---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming : ",CP)
